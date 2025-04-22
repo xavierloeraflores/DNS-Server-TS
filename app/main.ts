40985a46b39f6f8cab08395a5106d041e92f8be6
Xavier Loera Flores <x.loera.tech@gmail.com>
2025-04-21 17:58:02 -0700

    created CreateQuestionSectionBuffer function to respond with reply packets containing the DSN question section

--- app/main.ts
+++ app/main.ts
@@ -99,3 +99,91 @@ const createDNSHeader = ({
     console.log(byteArray);
     return byteArray;
 };
+
+
+enum QuestionType {
+    A=1, //Host Address
+    NS=2, //Authoritative Name Server
+    MD=3, //Mail Destination
+    MF=4, // Mail Forwarder
+    CNAME=5, // Canonical Name for Alias
+    SOA=6, //Start of Zone of Authority
+    MB=7, //Mailbox Domain Name
+    MG=8, //Mail Group Member 
+    MR=9, //Mail Rename Domain Name
+    NULL=10, //Null
+    WKS=11, //Well Known Service Description
+    PTR=12, //Domain Name Pointer
+    HINFO=13, //Host Info
+    MINFO=14, //Mailbox/Mail-List Info
+    MX=15, // Mail Exchange
+    TXT=16 //Text String
+}
+
+enum QuestionClass {
+    IN=1, //Internet
+    CS=2, //CSNET
+    CH=3, //Chaos
+    HS=4 //Hesiod
+}
+
+
+/**
+ * 
+ * @param name Requested Resource NAME - variable bytes 
+ * @param type Requested Resource TYPE - 2 bytes
+ * @param classCode CLASS Code - 2 bytes
+ */
+const createQuestionSectionBuffer = ({name, type, classCode}:{
+    name:string,
+    type:QuestionType,
+    classCode:QuestionClass
+})=>{
+
+
+    // Domains may have multiple labels separated by a "."
+    const labels = name.split(".")
+    // Each label must then be added to the Requested Resource Name buffer
+    // as such <length><content> for every label and finally with a null byte 
+    
+    let nameBufferLength = 0
+    const labelBuffers:Array<Array<number>>= []
+    labels.forEach(label=>{
+        const curBuffer:Array<number> = []
+        for(let i =0; i<label.length; i++ ){
+            const curChar = label.charCodeAt(i)
+            curBuffer.push(curChar)
+        }
+        labelBuffers.push(curBuffer)
+        nameBufferLength += curBuffer.length + 1
+    })
+
+
+const nameBuffer = new Uint8Array(nameBufferLength+1)
+let curByte = 0
+
+labelBuffers.forEach(labelBuffer=>{
+    const labelBufferLength = labelBuffer.length
+    nameBuffer[curByte] = labelBufferLength & 0xff
+    curByte++
+    labelBuffer.forEach(labelByte=>{
+     nameBuffer[curByte] = labelByte
+     curByte++   
+    })
+})
+
+nameBuffer[nameBuffer.length -1] = 0x00 //Null Byte to terminate name labels sequence
+
+
+//Create the buffer containing the Requested Resource Type & Class
+const highTypeByte = (type >> 8) & 0xff
+const lowTypeByte = type & 0xff
+const highClassByte = (classCode >> 8) & 0xff
+const lowClassByte = classCode & 0xff
+const TypeAndClassCodeBuffer = new Uint8Array([highTypeByte, lowTypeByte, highClassByte, lowClassByte])
+
+const QuestionSectionBuffer = Buffer.concat([nameBuffer, TypeAndClassCodeBuffer])
+
+console.log(QuestionSectionBuffer)
+return QuestionSectionBuffer
+}
