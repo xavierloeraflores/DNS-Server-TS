2edaf0774f5b05bf2ebd0c850a998b74ba33178d
Xavier Loera Flores <x.loera.tech@gmail.com>
2025-04-22 23:45:30 -0700

    Added AnswerBuffer in DNS reply packet

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,7 @@ import * as dgram from "dgram";
 import { ResourceRecordClass, ResourceRecordType } from "./enums";
 import { buildHeaderBuffer } from "./header";
 import { buildQuestionBuffer } from "./question";
+import { buildAnswerBuffer } from "./answer";
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
@@ -9,13 +10,26 @@ udpSocket.bind(2053, "127.0.0.1");
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
   try {
     console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-    const header = buildHeaderBuffer({ ID: 1234, QR: true, QD_COUNT: 1 });
+    const header = buildHeaderBuffer({
+      ID: 1234,
+      QR: true,
+      QD_COUNT: 1,
+      AN_COUNT: 1,
+    });
     const question = buildQuestionBuffer({
       name: "codecrafters.io",
       type: ResourceRecordType.A,
       classCode: ResourceRecordClass.IN,
     });
-    const response = Buffer.concat([header, question]);
+    const answer = buildAnswerBuffer({
+      name: "codecrafters.io",
+      type: ResourceRecordType.A,
+      classCode: ResourceRecordClass.IN,
+      ttl: 60,
+      length: 4,
+      data: "8.8.8.8",
+    });
+    const response = Buffer.concat([header, question, answer]);
     console.log({ response });
 
     udpSocket.send(response, remoteAddr.port, remoteAddr.address);
