ca3c02a2eb67d4871aec09d0a404a065c48b6cf2
Xavier Loera Flores <x.loera.tech@gmail.com>
2025-04-22 19:40:38 -0700

    Refactored to move reused smaller buffer builder functions into a separate file

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,10 @@
 import * as dgram from "dgram";
 import { ResourceRecordClass, ResourceRecordType } from "./enums";
+import {
+  createTTLBuffer,
+  createNameBuffer,
+  createTypeAndClassCodeBuffer,
+} from "./buffers";
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
@@ -133,72 +138,6 @@ const createQuestionSectionBuffer = ({
 };
 
 /**
- * @param name Requested Resource NAME - variable bytes
- * @returns NameBuffer - variable bytes
- */
-function createNameBuffer({ name }: { name: string }) {
-  // Domains may have multiple labels separated by a "."
-  const labels = name.split(".");
-  // Each label must then be added to the Requested Resource Name buffer
-  // as such <length><content> for every label and finally with a null byte
-
-  let nameBufferLength = 0;
-  const labelBuffers: Array<Array<number>> = [];
-  labels.forEach((label) => {
-    const curBuffer: Array<number> = [];
-    for (let i = 0; i < label.length; i++) {
-      const curChar = label.charCodeAt(i);
-      curBuffer.push(curChar);
-    }
-    labelBuffers.push(curBuffer);
-    nameBufferLength += curBuffer.length + 1;
-  });
-
-  const nameBuffer = new Uint8Array(nameBufferLength + 1);
-  let curByte = 0;
-
-  labelBuffers.forEach((labelBuffer) => {
-    const labelBufferLength = labelBuffer.length;
-    nameBuffer[curByte] = labelBufferLength & 0xff;
-    curByte++;
-    labelBuffer.forEach((labelByte) => {
-      nameBuffer[curByte] = labelByte;
-      curByte++;
-    });
-  });
-
-  nameBuffer[nameBuffer.length - 1] = 0x00; //Null Byte to terminate name labels sequence
-  return nameBuffer;
-}
-
-/**
- * @param type Requested Resource TYPE - 2 bytes
- * @param classCode CLASS Code - 2 bytes
- * @returns TypeAndClassCodeBuffer - 4 bytes
- */
-function createTypeAndClassCodeBuffer({
-  type,
-  classCode,
-}: {
-  type: ResourceRecordType;
-  classCode: ResourceRecordClass;
-}) {
-  //Create the buffer containing the Requested Resource Type & Class
-  const highTypeByte = (type >> 8) & 0xff;
-  const lowTypeByte = type & 0xff;
-  const highClassByte = (classCode >> 8) & 0xff;
-  const lowClassByte = classCode & 0xff;
-  const TypeAndClassCodeBuffer = new Uint8Array([
-    highTypeByte,
-    lowTypeByte,
-    highClassByte,
-    lowClassByte,
-  ]);
-
-  return TypeAndClassCodeBuffer;
-}
-
-/**
  *
  * @param name Requested Resource NAME - variable bytes
  * @param type Requested Resource TYPE - 2 bytes
@@ -236,16 +175,3 @@ const createAnswerSectionBuffer = ({
   ]);
   return AnswerBuffer;
 };
-
-/**
- * @param ttl Time To Live - 4 bytes
- * @returns TTLBuffer - 4 bytes
- */
-function createTTLBuffer({ ttl }: { ttl: number }) {
-  const TTLBuffer = new Uint8Array(4);
-  TTLBuffer[0] = (ttl >> 24) & 0xff;
-  TTLBuffer[1] = (ttl >> 16) & 0xff;
-  TTLBuffer[2] = (ttl >> 8) & 0xff;
-  TTLBuffer[3] = (ttl >> 0) & 0xff;
-  return TTLBuffer;
-}
